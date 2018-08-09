a0b6b68404bd8d78ece97bf0f8e3b86eddeb61f5
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-09 13:32:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,8 @@ class Application
 
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
+    elsif num_1==num_2 || num_2==num_3
+      resp.write "Only 2 numbers match. U still sukk"
     else
       resp.write "You Lose"
     end
